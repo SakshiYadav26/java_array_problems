8e8e936043e00e329e1608085ac0746c28b1db01
Sakshi Yadav <sakshiyadav470@gmail.com>
2025-03-23 23:20:31 +0530

    Create cells with Odd Values in a Matrix.java

---
+++ ells with Odd Values in a Matrix.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/description/
+class Solution {
+    public int oddCells(final int n, final int m, final int[][] indices) {
+        int count = 0;
+        final int[] col = new int[m], row = new int[n];
+
+        for(int[] x : indices) {
+            row[x[0]]++;
+            col[x[1]]++;
+        }
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if((row[i] + col[j]) % 2 != 0)
+                    count++;
+
+        return count;
+    }
+}
